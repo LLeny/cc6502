108e3a519ff8384b6cafe8dd18ea56dcd342d309
Bruno STEUX <B.STEUX@nexter-group.fr>
2026-02-09 11:36:38 +0100

    fixed bug with comparison between a[X] and Y. Added if_test13.

--- src/generate/generate_conditions.rs
+++ src/generate/generate_conditions.rs
@@ -628,7 +628,7 @@ impl<'a, 'b> GeneratorState<'a> {
                         return self.generate_condition_ex(
                             &ExprType::A(false),
                             op,
-                            r,
+                            right,
                             pos,
                             negate,
                             label,
@@ -681,7 +681,7 @@ impl<'a, 'b> GeneratorState<'a> {
                         return self.generate_condition_ex(
                             &ExprType::A(false),
                             op,
-                            r,
+                            right,
                             pos,
                             negate,
                             label,
